12f76b3bc4ba21fd441260d076ce166bcd82c197
Rebun13 <rebun@tutamail.com>
2024-12-19 21:30:17 +0100

    Created remaining views No content on them. Ruben Gomez Dopazo

---
+++ app/studies/page.tsx
@@ -0,0 +1,5 @@
+export default async function Studies() {
+    return (
+        <code>Work in progress</code>
+    )
+}
---
+++ app/work-experience/page.tsx
@@ -0,0 +1,5 @@
+export default async function WorkExperience() {
+    return (
+        <code>Work in progress</code>
+    )
+}
