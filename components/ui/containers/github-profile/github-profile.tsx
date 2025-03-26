1a7ff03c55ece7baeb39698e79a9b0f2f72b63bb
Ruben Gomez Dopazo <rebun@tutamail.com>
2025-03-26 21:04:06 +0100

    Finished github repositories Styling and filtering implemented. Ruben Gomez Dopazo

--- components/ui/containers/github-profile/github-profile.tsx
+++ components/ui/containers/github-profile/github-profile.tsx
@@ -2,10 +2,24 @@
 
 import { GithubRepository } from "@/components/types";
 import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+
+import styles from "./styles.module.css";
+import 'react-loading-skeleton/dist/skeleton.css';
+import { v4 as uuid } from "uuid";
 
 export default function Repos() {
     const [response, setResponse] = useState<Array<GithubRepository>>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    const [filterBy, setFilterBy] = useState<string | null>(null);
+    const filterRepos = (topic: string) => {
+        if (filterBy === topic) {
+            setFilterBy(null);
+        } else {
+            setFilterBy(topic);
+        }
+    };
 
     const headers = new Headers();
     headers.set('Accept', 'application/vnd.github+json');
@@ -17,29 +31,56 @@ export default function Repos() {
             method: 'GET',
             headers: headers
         };
-        fetch('https://api.github.com/users/Rebun13/repos', requestOptions)
-          .then((res) => res.json())
-          .then((data: Array<GithubRepository>) => {
-            setResponse(data);
-            setIsLoading(false);
-          });
+        fetch('https://api.github.com/users/Rebun13/repos?type=public&per_page=100', requestOptions)
+            .then((res) => res.json())
+            .then((data: Array<GithubRepository>) => {
+                setResponse(data);
+                setIsLoading(false);
+            });
     }, []);
 
-    if (isLoading) return <p>Loading...</p>;
-    if (!response) return <p>No profile data</p>;
-    console.log(response[0]);
+    if (isLoading) return (
+        <div className={styles.tableSkeleton}>
+            <Skeleton count={1} height={40} baseColor="rgba(var(--foreground), .5)" highlightColor="rgba(var(--foreground), .8)" />
+            <Skeleton count={7} height={30} baseColor="rgba(var(--foreground), .5)" highlightColor="rgba(var(--foreground), .8)" />
+        </div>
+    );
+    if (!response) return <p>Could not fetch my projects 🤔</p>;
+
     return (
-        <div>
-            <h1>Public Repositories</h1>
-            <ul>
-                {response.map((repo) => (
-                    <li key={repo.id}>
-                        <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                            {repo.name}
-                        </a>
-                    </li>
-                ))}
-            </ul>
+        <div className={styles.tableWrapper}>
+            <table className={styles.table}>
+                <thead className={styles.thead}>
+                    <tr>
+                        <th>Repo</th>
+                        <th>Language</th>
+                        <th>Description</th>
+                        <th>Tags</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {response.map((repo) => {
+                        if (filterBy !== null && !repo.topics?.includes(filterBy)) return <></>;
+                        return (
+                            <tr key={repo.id} className={styles.trow}>
+                                <td>
+                                    <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                                        {repo.name}
+                                    </a>
+                                </td>
+                                <td>{repo.language}</td>
+                                <td>{repo.description}</td>
+                                <td>{repo.topics?.map((topic) => (
+                                    <button
+                                        key={uuid()}
+                                        className={topic === filterBy ? `${styles.topic} ${styles.selected}` : styles.topic}
+                                        onClick={() => filterRepos(topic)}
+                                    >{topic}</button>))}
+                                </td>
+                            </tr>)
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
