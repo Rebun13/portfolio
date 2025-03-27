64432d77b8697c520614f1b1696ac757e1189851
Rebun13 <rebun@tutamail.com>
2025-03-27 17:05:52 +0100

    Corrected navbar behaviour Now it works as expected when clicking and bluring it. Ruben Gomez Dopazo

--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Logo } from "@/components/ui/logos/Logos";
 import styles from "./styles.module.css";
+import { useEffect, useRef, useState } from "react";
 
 interface Element {
     label: string
@@ -12,6 +13,23 @@ interface Element {
 }
 
 export function Navbar() {
+    const [isHovered, setIsHovered] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClick = (event: MouseEvent) => {
+        if (ref.current && !ref.current.contains(event.target as Node)) {
+            setIsHovered(false);
+        }
+        };
+
+        document.addEventListener("click", handleClick, true);
+
+        return () => {
+        document.removeEventListener("click", handleClick, true);
+        };
+    }, [ref, isHovered]);
+
     const pathname = usePathname()
     const links: Element[] = [
         {
@@ -33,8 +51,11 @@ export function Navbar() {
     ]
 
     return (
-        <header
-            className={`${accentFont.className} ${styles.navbar}`}
+        <div
+            className={`${accentFont.className} ${styles.navbar} ${isHovered ? styles.active : ""}`}
+            onClick={() => setIsHovered(!isHovered)}
+            onBlur={() => setIsHovered(false)}
+            ref={ref}
         >
             <Logo />
             {links.map((link: Element) =>
@@ -47,6 +68,6 @@ export function Navbar() {
                 </Link>
 
             )}
-        </header>
+        </div>
     )
 }
